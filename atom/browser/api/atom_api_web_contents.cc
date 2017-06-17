cb2014f9e8ae0283e5f20d4e2167be1592228887
Aleksei Kuzmin <alkuzmin@microsoft.com>
2017-06-17 03:41:58 +0300

    Adds a basic offline check to InstallableManager.
    https://codereview.chromium.org/2751343002

--- atom/browser/api/atom_api_web_contents.cc
+++ atom/browser/api/atom_api_web_contents.cc
@@ -1223,7 +1223,7 @@ void WebContents::InspectServiceWorker() {
 }
 
 void WebContents::HasServiceWorker(
-    const base::Callback<void(bool)>& callback) {
+    const base::Callback<void(content::ServiceWorkerCapability)>& callback) {
   auto context = GetServiceWorkerContext(web_contents());
   if (!context)
     return;
